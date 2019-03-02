64881d671f61111a734c186eb7b3a617ac413996
murat.doner <murat.doner@atg.se>
2019-03-02 14:43:46 +0100

    Navbar and "Ask for help" button added.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import ItemList from "./ItemList";
 import ProblemTitleForm from "./ProblemTitleForm";
-import {Col, Row} from "reactstrap";
+import {Col, Row, Button} from "reactstrap";
 import ContentList from "./ContentList";
 import {Route} from "react-router-dom";
+import { FaHireAHelper} from 'react-icons/fa';
 
-import {Link} from 'react-router-dom';
-import {properties} from "../config/properties";
 import * as Keycloak from "keycloak-js";
-const keycloak = Keycloak('/keycloak.json');
+import ModalExample from "./ModalExample";
+import NavbarHa from "./NavbarHa";
+ const keycloak = Keycloak('/keycloak.json');
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = { keycloak: null, authenticated: false };
+        this.state = { keycloak: null, authenticated: false, modal: false };
+        this.toggle = this.toggle.bind(this);
     }
 
     componentDidMount() {
@@ -41,7 +43,15 @@ export default class Home extends Component {
 
         keycloak.logout();
     }
+    toggle() {
+        this.setState(prevState => ({
+            modal: !prevState.modal
+        }));
+    }
     render() {
+
+        const externalCloseBtn = <a className="nav-link" href="#" onClick={this.toggle}>Ask for Help <FaHireAHelper /></a>;
+        const buttonLabel ="Ask for Help"
 
          if(this.state.keycloak) {
              if(this.state.authenticated) return (
@@ -58,12 +68,15 @@ export default class Home extends Component {
                  </div>
              ); else return (
                  <div>
+                     <NavbarHa externalCloseBtn={externalCloseBtn}/>
                      <button onClick={this.handleLogin}>Login</button>
                        <Row>
                          <Col xs="6" sm="4"><ItemList /></Col>
                          <Col xs="6" sm="8"><Route exact path="/contents/:title" component={ContentList} /></Col>
                      </Row>
 
+
+                     <ModalExample externalCloseBtn={externalCloseBtn} buttonLabel={buttonLabel} modal={this.state.modal}  toggle={this.toggle} />
                  </div>
              )
          }
