80e83bbb1ff2efccd89a89b3d8661af1380ba56f
murat.doner <murat.doner@atg.se>
2019-03-12 14:38:18 +0100

    Pagination added to Problem List

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,15 +6,23 @@ import {Link} from 'react-router-dom';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import { properties } from '../config/properties.js';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
+
+  var amount=30;
 class ItemList extends Component {
+
     componentDidMount() {
 
-        this.props.fetchData(properties.serverUrl + properties.getAllTitles);
+        this.props.fetchData(properties.serverUrl + properties.getAllTitles + "/"+ amount);
     }
     handleChangeSearchCriteriaEvent(title, event)   {
       this.props.changeSearchCriteria(title);
     }
+
+    fetchMoreData = () => {
+        this.props.fetchData(properties.serverUrl + properties.getAllTitles+ "/"+ (10 + amount));
+     };
     render() {
         if (this.props.hasErrored) {
             return <p>Sorry! There was an error loading the items</p>;
@@ -23,21 +31,34 @@ class ItemList extends Component {
         if (this.props.isLoading) {
             return <p>Loading…</p>;
         }
-
+        const list= <ListGroup>
+            {this.props.items.map((item, index) => (
+                <ListGroupItem  key={item.id}> <Link onClick={(e) => this.handleChangeSearchCriteriaEvent(item.name, e)} to={{
+                    pathname: '/contents/' + item.name,
+                    state: {
+                        name: item.name
+                    }
+                }} > {item.name}</Link>
+                </ListGroupItem>
+            ))}
+        </ListGroup>;
         return (
             <div>
             <b>Problems:</b>
-                <ListGroup>
-                {this.props.items.map((item) => (
-                    <ListGroupItem  key={item.id}> <Link onClick={(e) => this.handleChangeSearchCriteriaEvent(item.name, e)} to={{
-                        pathname: '/contents/' + item.name,
-                        state: {
-                            name: item.name
-                        }
-                    }} > {item.name}</Link>
-                    </ListGroupItem>
-                   ))}
-            </ListGroup>
+
+
+                <div id="scrollableDiv" style={{ height: 700, overflow: "auto" }}>
+
+                <InfiniteScroll
+                        dataLength={this.props.items.length}
+                        next={this.fetchMoreData}
+                        hasMore={true}
+                        loader={<br/>}
+                        scrollableTarget="scrollableDiv"
+                         >
+                        {list}
+                    </InfiniteScroll>
+                </div>
             </div>
         );
     }
