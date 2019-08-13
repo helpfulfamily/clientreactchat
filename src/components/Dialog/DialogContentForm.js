fdd05d721866fde12d1b24960570b4d6096688fa
yolcura1@gmail.com <Tahminen12>
2019-08-13 23:06:13 +0300

    Düzeltmeler ve yorumlar

---
+++ src/components/Dialog/DialogContentForm.js
@@ -0,0 +1,143 @@
+import React from 'react';
+
+import {properties} from '../../config/properties.js';
+import {Button} from "reactstrap";
+import {
+    publishProblem
+} from "../../actions/channel/ProblemTitleAction";
+import {connect} from "react-redux";
+import PropTypes from 'prop-types'
+import {convertToRaw, EditorState, ContentState} from "draft-js";
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from "draftjs-to-html";
+import {getToken} from "../common/process";
+
+class dialogContentForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            channel: '',
+            channelContent: '',
+            editorState: EditorState.createEmpty()
+        };
+
+
+        this.handleSubmitProcess = this.handleSubmitProcess.bind(this);
+
+        this.handleKeyCommand = this.handleKeyCommand.bind(this);
+
+    }
+
+    onEditorStateChange = (editorState) => {
+
+        this.setState({editorState: editorState});
+
+        this.setState({channelContent:  draftToHtml(convertToRaw(editorState.getCurrentContent()))});
+
+
+    };
+
+
+
+    handleSubmitProcess(event) {
+
+        if(typeof event!="undefined"){
+            event.preventDefault();
+        }
+
+
+        getToken(this.props.loginUser.sso.keycloak).then( (token) => this.startPublishProcess(token))
+            .catch(function(hata){
+
+                console.log(hata)
+            });
+
+
+
+
+    }
+    startPublishProcess = (token) =>
+    {
+
+        var apiBaseUrl = properties.channel_publishContent;
+
+
+
+        var item = {
+            "name": "",
+            "text": this.state.channelContent,
+            "channel": {
+                "name": decodeURIComponent(this.props.channelName)
+            }
+        }
+
+        this.clearForm();
+
+        this.props.postData(apiBaseUrl, item, token);
+    }
+    handleKeyCommand(command: string): DraftHandleValue {
+        if (command === 'split-block') {
+            // Perform a request to save your contents, set
+            // a new `editorState`, etc.
+            this.handleSubmitProcess();
+        }
+        return 'not-handled';
+    }
+
+
+    clearForm(){
+        const editorState = EditorState.push(this.state.editorState, ContentState.createFromText(''));
+        this.setState({editorState: editorState});
+    }
+    render() {
+        if((typeof this.props.loginUser.sso!=="undefined") && this.props.loginUser.sso.isAuthenticated){
+            return (
+                <div>
+
+
+                    <Editor
+                        editorState={this.state.editorState}
+                        wrapperClassName="demo-wrapper"
+                        editorClassName="demo-editor"
+                        onEditorStateChange={this.onEditorStateChange}
+                        handleKeyCommand={this.handleKeyCommand}
+                    />
+
+                    <Button color="primary" onClick={this.handleSubmitProcess}>Publish</Button>{' '}
+
+
+
+                    <p/>
+                </div>
+
+            );
+        }else{
+            return ("");
+        }
+
+    }
+}
+
+dialogContentForm.propTypes = {
+    postData: PropTypes.func.isRequired,
+    loginUser: PropTypes.object.isRequired,
+    hasErrored: PropTypes.bool.isRequired,
+    isLoading: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = (state) => {
+    return {
+        loginUser: state.loginReducer,
+        hasErrored: state.problemTitleHasErrored,
+        isLoading: state.problemTitleIsLoading
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+        postData: (url, item, token) => {console.log(url);  publishProblem(url, item, token)}
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelContentForm);
--- src/components/Dialog/DialogNameList.js
+++ src/components/Dialog/DialogNameList.js
@@ -7,28 +7,14 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import './channeltitle.css';
 import ThankcoinPanel from "../thankcoin/ThankcoinPanel";
 
-/*
 
-Kanal listesinin componentidir.
-Kanal listesi ile ilgili işlemler bu component üzerinden yapılır.
+class DialogNameList extends Component {
 
-
- */
-
-class ChannelNameList extends Component {
+    componentDidMount() {
 
 
-/*
+    }
 
-Transaction metoduna receiver ve objectID olmak üzere iki parametre girilmiştir.
-Thankcoin aktarma işlemi için gereken veriyi hazırlar.
-Örneğin ChannelContentList.Js içerisinde şu kod ile çağırılmıştır;
-
-<ThankcoinPanel transaction={ this.getTransaction(content.user.username, content.id)}
-currentThankAmount={content.currentThankAmount}/>
-
-
- */
 
     getTransaction(receiver, objectId) {
 
@@ -40,38 +26,23 @@ currentThankAmount={content.currentThankAmount}/>
             objectId: objectId,
             name: ""
         }
-        return transaction
+        return transaction;
 
     }
-
-
 
     render() {
         if (this.props.hasErrored) {
             return <p>Sorry! There was an error loading the items</p>;
         }
 
-        /*
-        Yükleme hatası olursa döndürelecek bir mesaj belirtilmiştir.
-
-        */
-
         if (this.props.isLoading) {
             return <p>Loading…</p>;
         }
-
-
-        /*
-
-        Yükleme sırasında döndürülecek mesaj belirtilmiştir.
-
-        */
-
         var list = "";
         var infiniteList="";
         if (typeof this.props.loginUser !== "undefined"
-                            && typeof this.props.loginUser.channels !== "undefined"
-                            && this.props.loginUser.channels.length > 0) {
+            && typeof this.props.loginUser.channels !== "undefined"
+            && this.props.loginUser.channels.length > 0) {
             list = <ListGroup className="problemtitle">
                 {this.props.loginUser.channels.map((item, index) => (
                     <ListGroupItem key={item.id}> <Link to={{
@@ -102,8 +73,7 @@ currentThankAmount={content.currentThankAmount}/>
         return (
             <div>
 
-                <div
-                    id="scrollableDiv" style={{height: 700, overflow: "auto"}}>
+                <div id="scrollableDiv" style={{height: 700, overflow: "auto"}}>
 
                     {infiniteList}
                 </div>
@@ -112,7 +82,7 @@ currentThankAmount={content.currentThankAmount}/>
     }
 }
 
-ChannelNameList.propTypes = {
+DialogNameList.propTypes = {
     loginUser: PropTypes.object.isRequired,
 };
 
@@ -123,4 +93,4 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {
-} )(ChannelNameList);
+} )(DialogNameList);
