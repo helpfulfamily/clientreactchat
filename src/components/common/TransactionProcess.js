bb0c248279dfd95342fb9c7777a01b48d52954e6
yolcura1@gmail.com <Tahminen12>
2019-08-15 21:22:05 +0300

    Oxios kütüphanesini anlamak

--- src/components/common/TransactionProcess.js
+++ src/components/common/TransactionProcess.js
@@ -1,22 +1,28 @@
 import axios from "axios";
 import {properties} from "../../config/properties";
 import {getToken} from "./process";
-// Transaction göndermek için token alıp, transaction gönderme işlemini çağıran fonksiyon.
+
+     // Transaction göndermek için token alıp, transaction gönderme işlemini çağıran fonksiyon.
+
 export   function getTokenForSendTransaction(keycloak, transaction)
 {
-    //Transaction yapmaya izin olup olmadığı anlaılsın diye, token alıyor.
+    //Transaction yapmaya izin olup olmadığı anlaşılsın diye, token alıyor.
     getToken(keycloak)
 
     // Token alındı ise, startTransaction fonksiyonu çağrılarak, transaction gönderme işlemi başlatılır.
+
         .then( (token) => startTransaction(token, transaction))
+
         // Eğer token alınırken bir hata oluştu ise, catch içerisinde o hata ekrana yazılıyor.
+
         .catch(function(hata){
 
             console.log(hata)
         });
 
 
-}
+}       //Aşağıda bearer + token ve headers değişkeni tanımlanıyor.
+
 function startTransaction(token, transaction){
     var bearer=  ' Bearer ' +   token;
     var headers = {
@@ -26,16 +32,26 @@ function startTransaction(token, transaction){
     }
 
 
+    /*
+
+    Axios bir kütüphanedir. Kütüphaneler, birçok componentin bulunduğu bir depo gibidir.
+    Axios ile get, post, delete, update gibi birçok işlemin daha kolay yapılmasını sağlar.
+    Burada ise axios, post isteği için kullanılmıştır.
+
+    */
 
     axios.post(properties.serverUrl+"/transaction/sendThankCoin", transaction, {headers: headers})
+
+
+         //(Response burada ne işe yarıyor anlamadım. Aldığı parametreye göre görevi değişiyor sanırım. Anlamadım.)
 
         .then( (response)  => {
             if (!response.status) {
                 throw Error(response.statusText);
-            }
+            } //  Eğer response edilemiyorsa hata verecek.
             if(response.data!==""){
 
-            }
+            } // Eğer response edilebiliyorsa datadan çekilen veri dönderilecek.
 
         })
         .catch( (error)  => {
