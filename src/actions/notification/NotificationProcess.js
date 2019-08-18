3ba95e41be9ffe44b3d0167c14218f3d5f025490
murat.doner <murat.doner@atg.se>
2019-08-18 13:25:08 +0200

    Transaction'un daha yalınlaşması için refactor.

---
+++ src/actions/notification/NotificationProcess.js
@@ -0,0 +1,50 @@
+import {notify} from 'reapop';
+import defaultavatar  from  "../../components/user/default-avatar.png";
+
+import { store } from '../../App'
+
+// Ekrana, o transaction işlemi ile ilgili notification mesajı çıkarır.
+export function  showNotificationForTransaction(transaction) {
+    var objectType=transaction.objectType;
+    var channelName= transaction.name;
+
+
+    var notificationMessage="You supported #" + channelName+ " channel!";
+
+    switch (objectType) {
+        case 'Channel':{
+
+            var image= transaction.receiver.profilePhotoUrl;
+            if(image==null){
+                image=defaultavatar;
+            }
+            console.log(defaultavatar);
+
+            /*
+                Bunu notify kütüphanesinin kullanılma şekli olarak kabul edelim. Notify, ekrana notification gösteren kütüphanedir.
+             */
+            store.dispatch(   notify({
+                title: "Thank you!",
+                message: notificationMessage,
+                image: image,
+                status: "success",
+                dismissible: false,
+                dismissAfter: 0,
+                buttons: [{
+                    name: 'OK',
+                    primary: true
+                } ],
+                allowHTML: true
+            }));
+
+
+            break;
+        }
+
+
+
+    }
+
+
+
+}
--- src/actions/websocket.js
+++ src/actions/websocket.js
@@ -2,7 +2,7 @@ import * as SockJS from 'sockjs-client';
 import * as Stomp from 'stompjs';
 import { properties } from '../config/properties.js';
 import dispatcherHaProblemContent from './channel/ProblemTitleAction';
-import dispatcherTransaction from './thankcoin/ThankcoinAction';
+import dispatcherTransaction from './thankcoin/TransactionProcess';
 import { dispatcherChannel } from './channel/ChannelAction';
 import dispatcherObservation from "./observation/ObservationAction";
 import { store } from '../App'
