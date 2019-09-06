1aca29d48a3be913d00feecefdaa090a44e24d41
murat.doner <murat.doner@atg.se>
2019-09-06 21:54:36 +0200

    TRANSACTION_CHANNEL_CONTENT

--- src/components/common/TransactionProcess.js
+++ src/components/common/TransactionProcess.js
@@ -1,6 +1,5 @@
-import axios from "axios";
-import {properties} from "../../config/properties";
 import {getToken} from "./process";
+import {transactionChannelContentOut} from "../../door/TransactionChannelContentDoor";
 
 
 /*
@@ -43,7 +42,7 @@ export   function getTokenForSendTransaction(keycloak, transaction)
 
     // Token alındı ise, startTransaction fonksiyonu çağrılarak, transaction gönderme işlemi başlatılır.
 
-        .then( (token) => startTransaction(token, transaction))
+        .then( (token) => transactionChannelContentOut(token, transaction))
 
         // Eğer token alınırken bir hata oluştu ise, catch içerisinde o hata ekrana yazılıyor.
 
@@ -53,52 +52,4 @@ export   function getTokenForSendTransaction(keycloak, transaction)
         });
 
 
-}
-
-//Aşağıda bearer + token ve headers değişkeni tanımlanıyor.
-function startTransaction(token, transaction){
-    var bearer=  ' Bearer ' +   token;
-    var headers = {
-        'Content-Type': 'application/json',
-        'Authorization': bearer,
-        'Access-Control-Allow-Origin': '*'
-    }
-
-
-    /*
-
-    Axios bir kütüphanedir. Kütüphaneler, birçok componentin bulunduğu bir depo gibidir.
-    Axios; get, post, delete, update gibi birçok işlemin daha kolay yapılmasını sağlar.
-    Burada ise axios, post isteği için kullanılmıştır zira Gateway modülündeki  method
-    aşağıdaki gibi POST türünde tanımlanmıştır:
-
-    @PostMapping(value = "/sendThankCoin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-
-    Bu post çağrısını Chrome-> Inspect -> Network kısmında izleyebilirsiniz.
-    Bunun için, herhangi bir Thank buttonuna basıp, Thankcoin transfer etmeniz yeterlidir.
-    */
-
-    axios.post(properties.serverUrl+"/transaction/sendThankCoin", transaction, {headers: headers})
-
-
-         //(Response burada ne işe yarıyor anlamadım. Aldığı parametreye göre görevi değişiyor sanırım. Anlamadım.)
-
-        .then( (response)  => {
-            if (!response.status) {
-                throw Error(response.statusText);
-            } //  Eğer response edilemiyorsa hata verecek.
-            if(response.data!==""){
-
-            } // Eğer response edilebiliyorsa datadan çekilen veri dönderilecek.
-
-        })
-        .catch( (error)  => {
-
-        })
-        .then( () =>  {
-
-
-
-
-        });
 }
